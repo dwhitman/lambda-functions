2a743a1e2ce37f400f3cc8f537bb3322d792d1ab
dwhitman <39740379+dwhitman@users.noreply.github.com>
2020-02-19 09:17:42 -0800

    Create Power_Off_RDS_instances.py

---
+++ Power_Off_RDS_instances.py
@@ -0,0 +1,19 @@
+import boto3
+
+def lambda_handler(event, context): 
+    rds_dbs = ['plantronicsmanager', 'plantronicsmanager-ap', 'plantronicsmanager-au', 'plantronicsmanager-eu', 'pltsystem']
+    #rds_dbs = ['plantronicsmanager-ap']
+
+    for db in rds_dbs:
+    
+        rds_client = boto3.client('rds')
+    
+        db_status = rds_client.describe_db_instances(DBInstanceIdentifier=db)
+        print ("RDS " + db + " is: " + db_status['DBInstances'][0]['DBInstanceStatus'])
+    
+        if 'available' in db_status['DBInstances'][0]['DBInstanceStatus']:
+            print ("- stopping " + db)
+            rds_client.stop_db_instance(DBInstanceIdentifier=db)
+        
+      
+lambda_handler(1,2)   
